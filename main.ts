f9b40c8e0edf8ac5cf346abffe6b388cc3d1e9e7
Brian McCann <bmccann36@gmail.com>
2022-02-16 09:10:43 -0500

    got the embedded subprocess to work

--- main.ts
+++ main.ts
@@ -1,5 +1,4 @@
-import {Handler, HandlerArgs} from "camunda-external-task-client-js";
-import {Client, logger} from "camunda-external-task-client-js";
+import {Client, HandlerArgs, logger} from "camunda-external-task-client-js";
 
 // configuration for the Client:
 //  - 'baseUrl': url to the Process Engine
@@ -10,10 +9,13 @@ const config = {baseUrl: "http://localhost:8080/engine-rest", use: logger};
 const client = new Client(config);
 
 // susbscribe to the topic: 'creditScoreChecker'
-client.subscribe("topic01", async function (handlerArgs: HandlerArgs) {
+client.subscribe("get-not-synced-purchases", async function (handlerArgs: HandlerArgs) {
     const {task, taskService} = handlerArgs;
-    console.log('vars are: ')
-    console.log(task.variables.getAllTyped())
+
+    // console.log(task.variables.getTyped('listElement'))
+    console.log(task.variables.get('element'))
+    // const listVar = task.variables.getTyped('list')
+    // console.log(JSON.parse(listVar.value))
 
     await taskService.complete(task);
 });
